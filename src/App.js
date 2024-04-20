6dad9614184046e208cc002c4a8820a4564e7f1b
milanlemaire <71797228+milan517@users.noreply.github.com>
2024-04-20 17:10:45 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h2>Cow Details</h2>
+        <p>Cow ID: 50003</p>
+        <p>Owner: 0x0000000000000000000000000000000000000000</p>
+        <p>Birth Date: 24/02/2008</p>
+        <p>Birth Place: BESELARE</p>
+        <p>Gender: X</p>
+        <p>Mother ID: 980</p>
+        <p>Father ID: 7688</p>
+        <p>Slaughter Date: 0</p>
+        <p>Farm Location: DE HEMEL</p>
+        <p>Farm Owner: 0xE8FC494786F3034Fd413ae6287097615a4AE1Ed0</p>
+        <p>Fat Content: 55%</p>
+        <p>Served: No</p>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwagyuvleesvandeboer.nl%2Fons-verhaal%2F&psig=AOvVaw0hBz5IeuHG5oEJWb1FYXIp&ust=1713696526148000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNj-mJjP0IUDFQAAAAAdAAAAABAJ>" />
+      </div>
+  );
+}
+
+export default App;
